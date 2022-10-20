e8a72fdc560a466ddee55338f7710036bd102e67
Bárbara Vitória <102703166+BarbaraVit8@users.noreply.github.com>
2022-10-20 01:18:18 +0000

    Teste

---
+++ main.py
@@ -0,0 +1,15 @@
+# Meu primeiro projeto Python!!! 
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+# Quando quiser guardar uma String! (Frase)
+
+nome = "Barbara Vitoria"
+# Quando quiser guardar um numero inteiro
+idade = 19 
+# Exibir o meu nome ( que esta dentro da variavel nome)
+print(nome)
+
+# Quando quiser exibir a frase "MInha idade é " completando com o conteudo da varaivel idade
+print("Minha idade é "+str(idade))
