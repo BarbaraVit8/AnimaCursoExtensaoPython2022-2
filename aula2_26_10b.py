0bb7c8043ef2f2ea948f1836b1203a1876dcd829
Bárbara Vitória <102703166+BarbaraVit8@users.noreply.github.com>
2022-11-23 11:38:54 +0000

    Exemplo bacana de if elif e else...!

--- aula2_26_10b.py
+++ aula2_26_10b.py
@@ -2,7 +2,7 @@
 nome = input("Informe seu nome: ")
 nota = float(input("Digite sua nota: "))
 
-if (nota == 10):
+if (nota == 10): 
   print(f"{nome}, você é bichão, mesmo...")
 elif (nota >= 6 and nota < 10):
   print(f"{nome}, bom trabalho")
